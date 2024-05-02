1898c33f17f9c4b54d9e297bd38a0c68f9813967
arinsuda <28irin62@gmail.com>
2024-05-02 15:00:50 +0700

    createItemNotFoundException
    ItemNotFoundException to manage 404 Exception

---
+++ Backend/src/main/java/int221/sit/taskboard/exceptions/ItemNotFoundException.java
@@ -0,0 +1,12 @@
+package int221.sit.taskboard.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+//@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ItemNotFoundException extends ResponseStatusException {
+    public ItemNotFoundException(String message) {
+        super(HttpStatus.NOT_FOUND, message);
+    }
+}
+
