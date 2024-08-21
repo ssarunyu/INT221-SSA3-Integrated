f1d50ed99ac7697070833c9272c2110ebc1f7c6d
ssarunyu <ssarunyu04@gmail.com>
2024-08-21 22:38:39 +0700

    commit

--- Backend/src/main/java/int221/sit/taskboard/controller/AuthController.java
+++ Backend/src/main/java/int221/sit/taskboard/controller/AuthController.java
@@ -16,15 +16,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     @Autowired
