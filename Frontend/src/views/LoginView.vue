7592b8af86f0e41581fe16338629688a0c268299
Veerapat <veerapatasasaun@gmail.com>
2024-08-22 21:52:20 +0700

    update ui frontend

--- Frontend/src/views/LoginView.vue
+++ Frontend/src/views/LoginView.vue
@@ -15,8 +15,7 @@ const userLogin = async () => {
   try {
     const res = await postLogin('http://localhost:8080/login', user.value)
 
-    console.log(res)
-    if (res.ok) {
+    if (res.success) {
       router.push('/task')
     } else {
       userIncorrect.value = "Username or Password is incorrect"
@@ -29,27 +28,43 @@ const userLogin = async () => {
 </script>
 
 <template>
-  <div
-    class="flex items-center justify-center h-screen bg-blue-200 bg-opacity-75 backdrop-blur-md overflow-hidden bg-blend-hard-light">
+  <div class="grid grid-cols-2 h-screen">
+    <div class="flex items-center justify-center bg-white backdrop-blur-md">
+      <div class="p-8 max-w-md w-full">
+        <div class="mb-6">
+          <!-- title -->
+          <h1 class="font-bold mb-2 text-lg text-center ">Sign in Taskboard</h1>
+          <!-- Username -->
+          <div>
+            <p>Username</p>
+            <input v-model="username" class="itbkk-username rounded-md p-2 border border-gray-400 w-full mb-4"
+              placeholder="Username" type="text" required />
+          </div>
 
-    <div class="fiexd card-content p-4 rounded-box shadow-lg border border-gray-300 bg-white backdrop-blur">
-      <!-- title -->
-      <p class="font-bold mb-2 text-lg text-center ">Sign in Taskboard</p>
-      <!-- Username -->
-      <div>
-        <input v-model="username" class="itbkk-username rounded-lg p-2 border border-black w-full mb-4"
-          placeholder="Username" type="text" required />
+          <!-- Password -->
+          <div>
+            <p>Password</p>
+            <input v-model="password" class="itbkk-password rounded-md p-2 border border-gray-400 w-full mb-4"
+              placeholder="Password" type="password" required />
+          </div>
+          <!-- Button Login -->
+          <div>
+            <button @click="userLogin"
+              class="bg-green-500 itbkk-button-signin font-bold rounded-md w-full py-2 text-white hover:bg-green-600">
+              Login
+            </button>
+          </div>
+          <!-- Message Incorrect -->
+
+            <p v-if="userIncorrect" class=" text-red-500 mt-2">{{ userIncorrect }}</p>
+
+        </div>
+
       </div>
-
-      <!-- Password -->
-      <input v-model="password" class="itbkk-password rounded-lg p-2 border border-black w-full mb-4"
-        placeholder="Password" type="password" required />
-      <!-- Button Login -->
-      <button @click="userLogin"
-        class="bg-green-600 itbkk-button-signin border rounded-3xl w-full py-2 text-white hover:bg-green-500">
-        Login
-      </button>
-      <p v-if="userIncorrect" class="text-red-500 text-center mt-2">{{ userIncorrect }}</p>
+    </div>
+    <div class="hidden md:block">
+      <!-- <p class="bg-black h-full w-full object-cover"></p> -->
+      <img src="../assets/555.png" alt="sky" class="h-full w-full object-cover">
     </div>
   </div>
 </template>
